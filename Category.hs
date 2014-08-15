2f8b3e481fe919bf7408874aed16190ff9be9633
Ian Milligan <ianmllgn@gmail.com>
2014-08-15 10:24:40 -0700

    remove default definition for Object

--- Category.hs
+++ Category.hs
@@ -9,10 +9,10 @@ import GHC.Prim
 
 class Category m where
     type Object (m :: k -> k -> *) (a :: k) :: Constraint
-    type Object m a = ()
     id :: m a a
     (.) :: m b c -> m a b -> m a c
 
 instance Category (->) where
+    type Object (->) a = ()
     id = P.id
     (.) = (P..)
