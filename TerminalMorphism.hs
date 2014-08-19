55163e7200ef942b86fb252e6f466d7c7db4ffeb
Ian Milligan <ianmllgn@gmail.com>
2014-08-18 22:27:27 -0700

    add tags to TerminalMorphism to avoid ambiguity

--- TerminalMorphism.hs
+++ TerminalMorphism.hs
@@ -6,10 +6,11 @@
 module TerminalMorphism where
 
 import qualified Prelude as P
+import Data.Tagged
 
 import Category
 import Functor
 
 class (Functor f c1 c2, Object (Domain f c1) a, Object (Codomain f c2) x) => TerminalMorphism f a x c1 c2 | f -> c1, f -> c2 where
-    terminalMorphism :: Codomain f c2 (FMap f a) x
-    terminalFactorization :: Object (Domain f c1) y => Codomain f c2 (FMap f y) x -> Domain f c1 y a
+    terminalMorphism :: Tagged '(f, a) (Codomain f c2 (FMap f a) x)
+    terminalFactorization :: Object (Domain f c1) y => Tagged f (Codomain f c2 (FMap f y) x -> Domain f c1 y a)
