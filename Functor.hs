07ec4c27d984b50117c7acac9549539281fa163d
Ian Milligan <ianmllgn@gmail.com>
2014-08-22 20:59:00 -0700

    Remove objectMap from Functor class definition

--- Functor.hs
+++ Functor.hs
@@ -11,8 +11,10 @@ class (Category (Domain f domain), Category (Codomain f codomain)) => Functor f 
     type Domain f domain :: o1 -> o1 -> *
     type Codomain f codomain :: o2 -> o2 -> *
     type FMap f (a :: o1) :: o2
-    objectMap :: Tagged '(f, a) (Object (Domain f domain) a :- Object (Codomain f codomain) (FMap f a))
     fmap :: f -> Domain f domain a b -> Codomain f codomain (FMap f a) (FMap f b)
+
+objectMap :: forall f c1 c2 a. Functor f c1 c2 => Tagged '(f, a) (Object (Domain f c1) a :- Object (Codomain f c2) (FMap f a))
+objectMap = Tagged (Sub (case observeObjects (proxy morphismMap (Proxy :: Proxy f) (id :: Domain f c1 a a)) of Dict -> Dict))
 
 data CompF f g c1 c2 c3 where
     (:.:) :: (Functor f c2 c3, Functor g c1 c2, Domain f c2 ~ Codomain g c2) => f -> g -> CompF f g c1 c2 c3
@@ -21,9 +23,6 @@ instance (Functor f c2 c3, Functor g c1 c2, Codomain g c2 ~ Domain f c2) => Func
     type Domain (CompF f g c1 c2 c3) c1 = Domain g c1
     type Codomain (CompF f g c1 c2 c3) c3 = Codomain f c3
     type FMap (CompF f g c1 c2 c3) (a :: o1) = (FMap f ((FMap g a) :: o2) :: o3)
-    objectMap :: forall a. Tagged '(CompF f g c1 c2 c3, a)
-        (Object (Domain (CompF f g c1 c2 c3) c1) a :- Object (Codomain (CompF f g c1 c2 c3) c3) (FMap (CompF f g c1 c2 c3) a))
-    objectMap = Tagged (proxy objectMap (Proxy :: Proxy '(f, FMap g a)) . proxy objectMap (Proxy :: Proxy '(g, a)))
     fmap (f :.: g) = fmap f . fmap g
 
 data CanonicalF f where
@@ -33,5 +32,4 @@ instance Functor (CanonicalF f) ('KProxy :: KProxy *) ('KProxy :: KProxy *) wher
     type Domain (CanonicalF f) 'KProxy = (->)
     type Codomain (CanonicalF f) 'KProxy = (->)
     type FMap (CanonicalF f) a = f a
-    objectMap = Tagged (Sub Dict)
     fmap CanonicalF = P.fmap
