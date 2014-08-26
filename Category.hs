c9009d9c3da0159707804f608027dc3b88337315
Ian Milligan <ianmllgn@gmail.com>
2014-08-26 16:38:05 -0700

    Category: add documentation

--- Category.hs
+++ Category.hs
@@ -4,18 +4,27 @@ import qualified Prelude as P
 import GHC.Prim
 import Data.Constraint
 
+-- |The class of categories @m@ with objects of kind @k@ which satisfy the
+-- constraint @Object m@. For any objects @a@ and @b@, the type @m a b@ is the
+-- type of morphisms with domain @a@ and codomain @b@.
 class Category (m :: k -> k -> *) where
+    -- |The set of objects.
     type Object m (a :: k) :: Constraint
+    -- |The Identity operator.
     id :: Object m a => m a a
-    observeObjects :: m a b -> Dict (Object m a, Object m b)
+    -- |The Composition operator.
     (.) :: m b c -> m a b -> m a c
+    -- |Morphisms are arrows between objects.
+    observeObjects :: m a b -> Dict (Object m a, Object m b)
 
+-- |The category Hask of functions between types.
 instance Category (->) where
     type Object (->) a = ()
     id = P.id
     observeObjects = P.const Dict
     (.) = (P..)
 
+-- |The category of entailment from 'constraints'
 instance Category (:-) where
     type Object (:-) a = ()
     id = refl
