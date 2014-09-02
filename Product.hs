f19c797623eaac4205cc7b448c9a9acb1f194b99
Ian Milligan <ianmllgn@gmail.com>
2014-09-01 20:46:46 -0700

    Products must be indexed by category

--- Product.hs
+++ Product.hs
@@ -13,27 +13,27 @@ import TerminalMorphism
 import Monoidal
 
 class Category c => ProductCategory (c :: k -> k -> *) where
-    type (><) (a :: k) (b :: k) :: k
-    univProduct :: forall (a :: k) (b :: k). Tagged '(c, a, b) ((Object c a, Object c b) :- TerminalMorphism (Diag c) (a >< b) '(a, b))
+    type Product c (a :: k) (b :: k) :: k
+    univProduct :: forall (a :: k) (b :: k). Tagged '(c, a, b) ((Object c a, Object c b) :- TerminalMorphism (Diag c) (Product c a b) '(a, b))
 
-proj1 :: forall a b c. (ProductCategory c, Object c a, Object c b) => Tagged b (c (a >< b) a)
+proj1 :: forall a b c. (ProductCategory c, Object c a, Object c b) => Tagged b (c (Product c a b) a)
 proj1 = Tagged p where
     p :><: _ = t
-    t :: (c :><: c) '(a >< b, a >< b) '(a, b)
-    t = proxy terminalMorphism (Proxy :: Proxy '(Diag c, a >< b)) \\ proxy univProduct (Proxy :: Proxy '(c, a, b))
+    t :: (c :><: c) '(Product c a b, Product c a b) '(a, b)
+    t = proxy terminalMorphism (Proxy :: Proxy '(Diag c, Product c a b)) \\ proxy univProduct (Proxy :: Proxy '(c, a, b))
 
-proj2 :: forall a b c. (ProductCategory c, Object c a, Object c b) => Tagged a (c (a >< b) b)
+proj2 :: forall a b c. (ProductCategory c, Object c a, Object c b) => Tagged a (c (Product c a b) b)
 proj2 = Tagged p where
     _ :><: p = t
-    t :: (c :><: c) '(a >< b, a >< b) '(a, b)
-    t = proxy terminalMorphism (Proxy :: Proxy '(Diag c, a >< b)) \\ proxy univProduct (Proxy :: Proxy '(c, a, b))
+    t :: (c :><: c) '(Product c a b, Product c a b) '(a, b)
+    t = proxy terminalMorphism (Proxy :: Proxy '(Diag c, Product c a b)) \\ proxy univProduct (Proxy :: Proxy '(c, a, b))
 
-(&&&) :: forall c a b1 b2. ProductCategory c => c a b1 -> c a b2 -> c a (b1 >< b2)
+(&&&) :: forall c a b1 b2. ProductCategory c => c a b1 -> c a b2 -> c a (Product c b1 b2)
 f &&& g
     | Dict <- observeObjects f, Dict <- observeObjects g
         = proxy terminalFactorization (Proxy :: Proxy (Diag c)) (f :><: g) \\ proxy univProduct (Proxy :: Proxy '(c, b1, b2))
 
-(***) :: forall c a1 a2 b1 b2. ProductCategory c => c a1 b1 -> c a2 b2 -> c (a1 >< a2) (b1 >< b2)
+(***) :: forall c a1 a2 b1 b2. ProductCategory c => c a1 b1 -> c a2 b2 -> c (Product c a1 a2) (Product c b1 b2)
 f *** g = proxy morphMap (Proxy :: Proxy (ProductF c)) (f :><: g)
 
 data ProductF c where ProductF :: ProductCategory c => ProductF c
@@ -41,7 +41,7 @@ data ProductF c where ProductF :: ProductCategory c => ProductF c
 instance ProductCategory (c :: k -> k -> *) => Functor (ProductF c) ('KProxy :: KProxy ((k, k) -> k)) where
     type Domain (ProductF c) = c :><: c
     type Codomain (ProductF c) = c
-    type FMap (ProductF c) (a :: (k, k)) = L a >< R a
+    type FMap (ProductF c) (a :: (k, k)) = Product c (L a) (R a) 
     morphMap :: forall (a :: (k, k)) (b :: (k, k)). Tagged (ProductF c)
         (Domain (ProductF c) a b -> Codomain (ProductF c) (FMap (ProductF c) a :: k) (FMap (ProductF c) b :: k))
     morphMap = Tagged m where
@@ -57,7 +57,7 @@ instance TerminalMorphism (Diag (->)) (a, b) '(a, b) where
     terminalFactorization  = Tagged (\(f :><: g) z -> (f z, g z))
 
 instance ProductCategory (->) where
-    type (><) a b = (a, b)
+    type Product (->) a b = (a, b)
     univProduct = Tagged (Sub Dict)
 
 instance TerminalMorphism (Diag (:-)) ((a :: Constraint), b) '(a, b) where
@@ -65,5 +65,5 @@ instance TerminalMorphism (Diag (:-)) ((a :: Constraint), b) '(a, b) where
     terminalFactorization = Tagged (\(f :><: g) -> Sub (Dict \\ f \\ g))
 
 instance ProductCategory (:-) where
-    type (><) a b = ((a, b) :: Constraint)
+    type Product (:-) a b = ((a, b) :: Constraint)
     univProduct = Tagged (Sub Dict)
